851547f0ebf10cead11efd11736c9b5959a2e17d
Bogdan <bogdan_dss@mail.ru>
2021-11-14 23:54:25 +0100

    lesson6

--- Lesson6/Lesson6.cpp
+++ Lesson6/Lesson6.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-// ///task2/////
+// ///task2//////
 template<typename _CharT, typename _Traits> inline std::basic_ostream<_CharT, _Traits>& endll(std::basic_ostream<_CharT, _Traits>& __os) {
 	return flush((__os.put(__os.widen('\n'))).put(__os.widen('\n')));
 }
